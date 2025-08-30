251d4c697a84eab92c313fd723050e3d0b588b62
JuanSalazar77 <salazarpazjuanjose@gmail.com>
2025-08-30 17:32:02 -0500

    Validacion del pedido para que solo funcione en el rol cliente, navegacion a la vista de pedidos del cliente y creacion de la columna que permite subir el comprobante

--- src/routes/pedidoRouter.ts
+++ src/routes/pedidoRouter.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { PedidoControllers } from "../controllers/PedidoController";
 import { handleInputErrors } from "../middleware/validation";
-import { getPedidoByIdValidation, createPedidoValidation, updatePedidoValidation, deletePedidoValidation, createPedidoFromCartValidation } from "../middleware/pedido";
+import { 
+    getPedidoByIdValidation, 
+    createPedidoValidation, 
+    updatePedidoValidation, 
+    deletePedidoValidation, 
+    createPedidoFromCartValidation 
+} from "../middleware/pedido";
+import { authenticateToken, isClient } from "../middleware/AuthMiddleware";
 
 const router = Router();
 
+// Aplica el middleware de autenticación a todas las rutas de este router
+router.use(authenticateToken);
+
+// Rutas GET protegidas
 router.get("/", PedidoControllers.getPedidosAll);
 
 router.get("/:id",
@@ -13,18 +24,22 @@ router.get("/:id",
     PedidoControllers.getPedidoById
 );
 
+// Rutas POST para crear pedidos (protegidas y solo para clientes)
 router.post("/",
+    isClient,
     createPedidoValidation,
     handleInputErrors,
     PedidoControllers.crearPedido
 );
 
 router.post("/crear-desde-carrito",
+    isClient,
     createPedidoFromCartValidation,
     handleInputErrors,
     PedidoControllers.crearPedidoDesdeCarrito
 );
 
+// Rutas PUT y DELETE (protegidas)
 router.put("/:id",
     updatePedidoValidation,
     handleInputErrors,
