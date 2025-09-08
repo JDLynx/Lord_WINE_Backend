0fc168fc2de0c20924fae76e3439cd6982099594
DavidRengifo12 <tuemail@example.com>
2025-09-08 08:13:06 -0500

    Actualizacion de los pedids creados desde el carrito

--- src/routes/pedidoRouter.ts
+++ src/routes/pedidoRouter.ts
@@ -1,40 +1,55 @@
 import { Router } from "express";
 import { PedidoControllers } from "../controllers/PedidoController";
 import { handleInputErrors } from "../middleware/validation";
-import { getPedidoByIdValidation, createPedidoValidation, updatePedidoValidation, deletePedidoValidation, createPedidoFromCartValidation } from "../middleware/pedido";
+// Importa los middlewares de autenticación y autorización
+import { authenticateToken, isClient } from "../middleware/AuthMiddleware";
+import {
+  getPedidoByIdValidation,
+  createPedidoValidation,
+  updatePedidoValidation,
+  deletePedidoValidation,
+  createPedidoFromCartValidation,
+} from "../middleware/pedido";
 
 const router = Router();
 
 router.get("/", PedidoControllers.getPedidosAll);
 
-router.get("/:id",
-    getPedidoByIdValidation,
-    handleInputErrors,
-    PedidoControllers.getPedidoById
+router.get(
+  "/:id",
+  getPedidoByIdValidation,
+  handleInputErrors,
+  PedidoControllers.getPedidoById
 );
 
-router.post("/",
-    createPedidoValidation,
-    handleInputErrors,
-    PedidoControllers.crearPedido
+router.post(
+  "/",
+  createPedidoValidation,
+  handleInputErrors,
+  PedidoControllers.crearPedido
 );
 
-router.post("/crear-desde-carrito",
-    createPedidoFromCartValidation,
-    handleInputErrors,
-    PedidoControllers.crearPedidoDesdeCarrito
+router.post(
+  "/crear-desde-carrito",
+  authenticateToken,
+  isClient,
+  createPedidoFromCartValidation,
+  handleInputErrors,
+  PedidoControllers.crearPedidoDesdeCarrito
 );
 
-router.put("/:id",
-    updatePedidoValidation,
-    handleInputErrors,
-    PedidoControllers.actualizarPedidoId
+router.put(
+  "/:id",
+  updatePedidoValidation,
+  handleInputErrors,
+  PedidoControllers.actualizarPedidoId
 );
 
-router.delete("/:id",
-    deletePedidoValidation,
-    handleInputErrors,
-    PedidoControllers.eliminarPedidoId
+router.delete(
+  "/:id",
+  deletePedidoValidation,
+  handleInputErrors,
+  PedidoControllers.eliminarPedidoId
 );
 
 export default router;
